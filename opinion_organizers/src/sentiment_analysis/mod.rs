f9f9d27a9d14f4ee96a7c0f39c0553516c77e138
Aethyar <aethyar@gmail.com>
2023-03-24 20:49:44 +0700

    Added possible functions with their type signatures

--- opinion_organizers/src/sentiment_analysis/mod.rs
+++ opinion_organizers/src/sentiment_analysis/mod.rs
@@ -1,7 +1,18 @@
+pub mod dataset;
 
 #[allow(dead_code)]
-pub fn not_main() {
-    println!("Bye, world!");
+fn is_negative_word(word: &str) -> bool {
+
+}
+
+#[allow(dead_code)]
+fn is_positive_word(word: &str) -> bool {
+
+}
+
+#[allow(dead_code)]
+pub fn get_sentiment_count(input: String) -> (usize, usize) {
+    
 }
 
 #[cfg(test)]
