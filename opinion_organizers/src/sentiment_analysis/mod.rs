ac83cc5c9731de099091a587c64f1e64bf9c947e
Aethyar <aethyar@gmail.com>
2023-03-25 10:52:46 +0700

    Fixed get_sentiment_count()
    - now uses regex to allow letters, digits, dashes, and asterisks (as used in the datasets)
    - turns the words into lowercase first before checking

--- opinion_organizers/src/sentiment_analysis/mod.rs
+++ opinion_organizers/src/sentiment_analysis/mod.rs
@@ -1,5 +1,6 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 use rayon::{str::ParallelString, prelude::ParallelIterator};
+use regex::Regex;
 
 mod dataset;
 
@@ -15,9 +16,13 @@ pub fn get_sentiment_counts(input: String) -> (usize, usize) {
     let positive_count = AtomicUsize::new(0);
     let negative_count = AtomicUsize::new(0);
 
+    let re = Regex::new(r"[^\w*-]").unwrap();
+
     input.par_split_whitespace().for_each(|word| {
-        if is_positive_word(word) { positive_count.fetch_add(1, Ordering::SeqCst); }
-        else if is_negative_word(word) { negative_count.fetch_add(1, Ordering::SeqCst); }
+        let clean_word = re.replace_all(word, "").to_lowercase();
+        print!("{}\n", clean_word);
+        if is_positive_word(&clean_word) { positive_count.fetch_add(1, Ordering::SeqCst); }
+        else if is_negative_word(&clean_word) { negative_count.fetch_add(1, Ordering::SeqCst); }
     });
 
     (positive_count.into_inner(), negative_count.into_inner())
@@ -30,5 +35,7 @@ mod tests {
     #[test]
     fn basic_test() {
         assert_eq!((3, 0), get_sentiment_counts("Wow, a good word: Nice.".to_owned()));
+        assert_eq!((0, 2), get_sentiment_counts("He's a sad drop-out.".to_owned()));
+        assert_eq!((0, 2), get_sentiment_counts("bull****, bull----".to_owned()));
     }
 }
