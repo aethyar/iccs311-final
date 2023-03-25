efd079d1db1b9daf008715891c5c92a4fa90701a
GitJotsarup <jotsarupn@gmail.comgit config --global user.email jotsarupn@gmail.com>
2023-03-25 23:39:03 +0700

    refining and returning as web_to_string as String

--- opinion_organizers/src/web_scraper/mod.rs
+++ opinion_organizers/src/web_scraper/mod.rs
@@ -11,28 +11,24 @@ use scraper::{Html, Selector};
 
 
 #[allow(dead_code)]
-pub fn web_to_string(url: &str) {
-    /* we will be web scraping multiple websites at the same time in parallel */
-
+pub fn web_to_string(url: &str) -> String {
     let req = reqwest::blocking::get(url).expect("Couldn't load the url"); // error message for when we cannot establish a connection
     // if it's a success you will not see the error message
 
-    let doc_body = Html::parse_document(&req.text().unwrap());
+    let doc_body = Html::parse_document(&req.text().unwrap());  // parsing the document itself
 
-    let review_selector = Selector::parse("div.content p").unwrap();
-    // in the selectors we have another opportunity to go parallel
-    // every review name starts with "A review by... ". we could iterate through these
-    // in parallel to get the information faster
-    // this is tough tho sinCe each review needs to be expanded
-    // finding the read more button and following the links would be a smarter choice
-    // then each link web scraped in parallel
+    let review_selector = Selector::parse("div.content p").unwrap();    // finding the specific content we want to scrape using either a class or html ID
+
+    let mut review_texts = Vec::new(); // this empty vector is created so it can later hold all of the texts for output
 
     for review_selector in doc_body.select(&review_selector){
         let review_text = review_selector.text().collect::<Vec<_>>();
-        for i in 0..review_text.len() {
-            println!("{}", review_text[i]);
+        for i in 0..review_text.len() { // iterator
+            //println!("{}", review_text[i]);
+            review_texts.push(review_text[i].to_string()); // pushing them into one
         }
     }
+    review_texts.join("") // joining all of them in 1 to return as a String
 }
 
 #[cfg(test)]
