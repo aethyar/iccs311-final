eb9f1545d05226aa2f071c3b04a6af3f46c2466d
GitJotsarup <jotsarupn@gmail.comgit config --global user.email jotsarupn@gmail.com>
2023-03-26 23:12:49 +0700

    Main

--- opinion_organizers/src/main.rs
+++ opinion_organizers/src/main.rs
@@ -7,7 +7,9 @@ mod sentiment_analysis;
 fn main() {
     // ask for user input for a link to process
 
-    web_to_string("https://www.themoviedb.org/review/58a231c5925141179e000674");  // replace with function in web_scraper that processes the link
+    let urls = &["https://www.themoviedb.org/review/58a231c5925141179e000674", "https://www.themoviedb.org/review/5d340e7a2f8d090388d21ff2"];
+    let vec_texts = web_to_string(urls);
+    println!("{:?}", vec_texts[1]);
     let test_string = "This is a sad test.";  // replace with web_scraper output
 
     let (pos, neg): (usize, usize) = get_sentiment_counts(test_string.to_owned());  // replace with parallel code if web_scraper output is a collection of Strings
