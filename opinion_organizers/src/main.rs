d15776f99c2ea7fc0ceb8abf15307a84c14744b5
Aethyar <aethyar@gmail.com>
2023-03-26 23:30:40 +0700

    Now properly added the analyze_review

--- opinion_organizers/src/main.rs
+++ opinion_organizers/src/main.rs
@@ -1,23 +1,33 @@
 use crate::sentiment_analysis::get_sentiment_counts;
 use crate::web_scraper::web_to_string;
 
-mod sentiment_analysis;
 mod web_scraper;
+mod sentiment_analysis;
+
+use rayon::prelude::*;
+
+fn analyze_review(url: &str) -> f64 {
+    web_to_string(url);  // replace test_arr
+
+    let test_arr = vec![  // replace with web_scraper output
+        "It's a contemplative adventure and an emotional exploration that captivated me from its opening moments...",
+        "While not all-together perfect, the film represents a monumental cinematic achievement that deserves to be placed high within the caliber of Nolan's filmography.",
+        "As spectacular as it is flawed."
+    ];
+
+    let (pos, neg) = test_arr
+        .par_iter()
+        .map(|&s| get_sentiment_counts(s.to_string()))
+        .reduce(|| (0, 0), |(pos_count1, neg_count1), (pos_count2, neg_count2)| { (pos_count1 + pos_count2, neg_count1 + neg_count2) } );
+
+    if pos == 0 { return neg as f64; }
+    else if neg == 0 { return pos as f64 }
+    else { return pos as f64 / neg as f64 }
+}
 
 fn main() {
     // ask for user input for a link to process
 
-    web_to_string("https://www.themoviedb.org/review/58a231c5925141179e000674"); // replace with function in web_scraper that processes the link
-    let test_string = "This is a sad test."; // replace with web_scraper output
-
-    let (pos, neg): (usize, usize) = get_sentiment_counts(test_string.to_owned()); // replace with parallel code if web_scraper output is a collection of Strings
-
-    // can make the output fancier later
-    if pos == 0 {
-        print!("{}", neg)
-    } else if neg == 0 {
-        print!("{}", pos)
-    } else {
-        print!("{}", pos / neg)
-    }
+    let ratio = analyze_review("https://www.themoviedb.org/review/58a231c5925141179e000674");
+    print!("{}", ratio)
 }
