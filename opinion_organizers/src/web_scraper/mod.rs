3747a2a0937e71577c2232045e8b2d2207be175e
GitJotsarup <jotsarupn@gmail.comgit config --global user.email jotsarupn@gmail.com>
2023-03-26 23:12:24 +0700

    Turned the web scraper into a parallel webscraper

--- opinion_organizers/src/web_scraper/mod.rs
+++ opinion_organizers/src/web_scraper/mod.rs
@@ -1,34 +1,41 @@
 extern crate reqwest;
 extern crate scraper;
+extern crate rayon;
 
 use scraper::{Html, Selector};
+use rayon::prelude::*;
 
 // use scraper::Selector;
 // use scraper::Html;
 
 // Web_scraping
-// website we'll be scraping from - https://www.themoviedb.org/
+// we'll be scraping from the individual reviews on - https://www.themoviedb.org/ only.
 
 
 #[allow(dead_code)]
-pub fn web_to_string(url: &str) -> String {
-    let req = reqwest::blocking::get(url).expect("Couldn't load the url"); // error message for when we cannot establish a connection
-    // if it's a success you will not see the error message
+pub fn web_to_string(urls: &[&str]) -> Vec<String> {    // we take in a Vec of links that can be variale in amount
 
-    let doc_body = Html::parse_document(&req.text().unwrap());  // parsing the document itself
+    urls.par_iter().map(|url| {
+        
+        let req = reqwest::blocking::get(*url).unwrap();     // .expect("Couldn't load the url"); // error message for when we cannot establish a connection
+        // if it's a success you will not see the error message
 
-    let review_selector = Selector::parse("div.content p").unwrap();    // finding the specific content we want to scrape using either a class or html ID
+        let doc_body = Html::parse_document(&req.text().unwrap());  // parsing the document itself
 
-    let mut review_texts = Vec::new(); // this empty vector is created so it can later hold all of the texts for output
+        let review_selector = Selector::parse("div.content p").unwrap();    // finding the specific content we want to scrape using either a class or html ID
 
-    for review_selector in doc_body.select(&review_selector){
-        let review_text = review_selector.text().collect::<Vec<_>>();
-        for i in 0..review_text.len() { // iterator
-            //println!("{}", review_text[i]);
-            review_texts.push(review_text[i].to_string()); // pushing them into one
+        let mut review_texts = Vec::new(); // this empty vector is created so it can later hold all of the texts for output
+
+        for review_selector in doc_body.select(&review_selector){
+            let review_text = review_selector.text().collect::<Vec<_>>();
+            for i in 0..review_text.len() { // iterator
+                //println!("{}", review_text[i]);
+                review_texts.push(review_text[i].to_string()); // pushing them into one
+            }
         }
-    }
-    review_texts.join("") // joining all of them in 1 to return as a String
+        review_texts.join("") // joining all of them in 1 to return as a String
+    })
+    .collect() // puts it into the vector
 }
 
 #[cfg(test)]
