e5bd6ddd8eb66b6b5df1d7c24c44527b1551ba2b
GitJotsarup <jotsarupn@gmail.comgit config --global user.email jotsarupn@gmail.com>
2023-03-27 22:48:04 +0700

    completing a main that computes the final result

--- opinion_organizers/src/main.rs
+++ opinion_organizers/src/main.rs
@@ -5,18 +5,12 @@ mod sentiment_analysis;
 
 fn analyze_review(url: &str) -> f64 {
     use rayon::iter::*;
-    
-    review_collection(url);  // replace test_arr
 
-    let test_arr = vec![  // replace with web_scraper output
-        "It's a contemplative adventure and an emotional exploration that captivated me from its opening moments...",
-        "While not all-together perfect, the film represents a monumental cinematic achievement that deserves to be placed high within the caliber of Nolan's filmography.",
-        "As spectacular as it is flawed."
-    ];
+    let test_arr = review_collection(url);
 
     let (pos, neg) = test_arr
         .par_iter()
-        .map(|&s| get_sentiment_counts(s.to_string()))
+        .map(|s| get_sentiment_counts(s.to_string()))
         .reduce(|| (0, 0), |(pos_count1, neg_count1), (pos_count2, neg_count2)| { (pos_count1 + pos_count2, neg_count1 + neg_count2) } );
 
     if pos == 0 { return neg as f64; }
